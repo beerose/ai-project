c9f6d1cbc25d140e6c0195d0d5ae667341f26b6c
Senello <matoz1995@gmail.com>
2017-12-21 16:56:36 +0100

    better location switch

---
+++ ai-project/AI_Project/Assets/Scripts/RoofHideScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofHideScript : MonoBehaviour
+{
+    private Material mat;
+    private Color col;
+    private float t;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        col = mat.color;
+    }
+    void LateUpdate()
+    {
+        if (GameController.Instace.currentBoard.transform != transform.parent)
+        {
+            t += 2*Time.deltaTime;
+            if (t <= 1) col.a = t;
+            mat.color = col;
+        }
+        else
+        {
+            t = 0;
+            col.a = 0;
+            mat.color = col;
+        }
+    }
+}
