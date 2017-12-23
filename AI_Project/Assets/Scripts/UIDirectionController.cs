86dc167bc94ac36d68662b6a5cfd7d8e365f8e22
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2017-12-23 23:46:31 +0100

    add health-checker

---
+++ AI_Project/Assets/Scripts/UIDirectionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDirectionController : MonoBehaviour {
+
+
+    public bool m_UseRelativeRotation = true;
+
+
+    private Quaternion m_RelativeRotation;
+
+
+    private void Start()
+    {
+        m_RelativeRotation = transform.parent.localRotation;
+    }
+
+
+    private void Update()
+    {
+        if (m_UseRelativeRotation)
+            transform.rotation = m_RelativeRotation;
+    }
+}
