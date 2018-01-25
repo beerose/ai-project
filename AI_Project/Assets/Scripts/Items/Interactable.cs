421d394ea7d46a84b38fd195ec11c678db4c4fb1
Senello <matoz1995@gmail.com>
2018-01-25 16:15:01 +0100

    Interactable

---
+++ AI_Project/Assets/Scripts/Items/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag.Equals("Player"))
+        {
+            Interact();
+        }
+       
+    }
+}
