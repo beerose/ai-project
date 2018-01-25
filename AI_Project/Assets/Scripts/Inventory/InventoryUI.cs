a5f94b54b48e169d7088973e482972fe22a41207
Senello <matoz1995@gmail.com>
2018-01-25 18:56:32 +0100

    Using and Remove Items from Inventory
    Press "b" or "i" to open Inventory

---
+++ AI_Project/Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Transform ItemsParent;
+    public GameObject inventoryUI;
+    private Inventory inventory;
+
+    private InventorySlot[] slots;
+	void Start ()
+	{
+	    inventory = Inventory.Instance;
+	    inventory.OnItemChangedCallback += UpdateUI;
+
+	    slots = ItemsParent.GetComponentsInChildren<InventorySlot>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Inventory"))
+	    {
+	        inventoryUI.SetActive(!inventoryUI.activeSelf);
+	    }
+	}
+    
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.Items.Count)
+            {
+                slots[i].AddItem(inventory.Items[i]);
+            }
+            else
+            {
+                slots[i].ClerSlot();
+            }
+        }
+    }
+}
