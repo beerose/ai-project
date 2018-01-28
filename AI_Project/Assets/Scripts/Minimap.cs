b490071c2df001f7c2db857f5e615ec7ba56463f
Senello <matoz1995@gmail.com>
2018-01-28 20:16:46 +0100

    Minimap

---
+++ AI_Project/Assets/Scripts/Minimap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+
+    public Transform player;
+
+    public Shader EffectShader;
+
+    void OnEnable()
+    {
+        GetComponent<Camera>().SetReplacementShader(EffectShader, "");
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+}
